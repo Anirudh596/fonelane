d282270b6ae3745c13896e66bf4479ff94f28c3d
Anirudh596 <anirudhconnect0@gmail.com>
2023-09-14 09:50:28 +0530

    some login changes

--- client/frontend/src/components/Account/LogIn/LogIn.jsx
+++ client/frontend/src/components/Account/LogIn/LogIn.jsx
@@ -1,10 +1,11 @@
 import { faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faFacebookF, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 function LogIn() {
   return (
     <>
-      <div className="flex h-[100vh] w-full relative ">
+      <div className="flex min-h-screen w-full relative">
         <div className="flex-1 background-image">
           <div className="h-1/12  p-5">
             <div
@@ -46,7 +47,21 @@ function LogIn() {
               />
             </div>
           </div>
-          <div className="h-full"></div>
+          <div className="max-h-screen flex justify-center items-center">
+            <div className=" flex flex-col justify-center items-center gap-2">
+              <p className="title  text-3xl font-semibold text-[#f]">Log in</p>
+              <p>Continues your browsing in just a minute!</p>
+              <div className="flex gap-2 border border-black rounded-[4px] px-[110px] py-3">
+                <FontAwesomeIcon className="text-[24px]" icon={faGoogle}/> continue with Google
+              </div>
+              <div className="flex gap-2 border border-black rounded-[4px] px-[115px] py-3">
+                <FontAwesomeIcon className="text-[24px]" icon={faFacebookF}/> continue with Google
+              </div>
+              <div className="w-[420px] flex justify-center items-center">
+                <hr className="w-1/2 h-px bg-gray-500 border-0"/><p className="p-3">or</p><hr  className="w-1/2 h-px bg-gray-500 border-0"/>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
