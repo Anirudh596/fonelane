2423310aef799f54b04f9f3b53c6f52c6e3f1920
Anirudh596 <anirudhconnect0@gmail.com>
2023-09-27 04:40:20 -0700

    created login popup

---
+++ client/src/components/Authantication/LoginPopup.jsx
@@ -0,0 +1,142 @@
+import { motion, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
+import BackBtn from "../BackBtn/BackBtn";
+
+// eslint-disable-next-line react/prop-types
+const LoginPopup = ({ isOpen, onClose }) => {
+  const popupVariants = {
+    hidden: { opacity: 0, scale: 0 },
+    visible: { opacity: 1, scale: 1 },
+  };
+
+  const controls = useAnimation();
+  const popupRef = useRef();
+
+  useEffect(() => {
+    if (isOpen) {
+      controls.start("visible");
+      const handleOutsideClick = (e) => {
+        if (popupRef.current && !popupRef.current.contains(e.target)) {
+          onClose();
+        }
+      };
+
+      document.addEventListener("mousedown", handleOutsideClick);
+
+      return () => {
+        document.removeEventListener("mousedown", handleOutsideClick);
+      };
+    } else {
+      controls.start("hidden");
+    }
+  }, [isOpen, onClose, controls]);
+
+  return (
+    <motion.div
+      className={`fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50`}
+      initial="hidden"
+      animate={controls}
+      variants={popupVariants}
+      transition={{ duration: 0.2 }}
+    >
+      <div className="relative w-full h-full md:w-2/3 md:h-3/4" ref={popupRef}>
+        <div className="flex justify-center items-center w-full h-full rounded-xl bg-white z-50 transform transition-transform ease-in-out duration-700">
+          <div className="absolute top-0 right-0 m-5 z-50">
+            <BackBtn func={onClose} className="text-base text-white md:text-black" />
+          </div>
+          <div className="w-full h-full flex flex-col md:flex-row mx-auto ">
+            <div className=" md:flex-1  md:w-full md:h-full background-image  md:rounded-l-xl z-0">
+              <p className="text-xl font-semibold text-white m-5">
+                Login/Signup
+              </p>
+            </div>
+            <div className="flex-grow bg-white rounded-r-2xl z-50">
+              <div className="w-full h-full flex md:flex-col justify-center md:items-center p-5">
+                <form action="" className="w-full p-5">
+                  <div className="flex flex-col justify-between md:items-center gap-5 relative">
+                    <div className="w-full">
+                      <label htmlFor="phone_id">Mobile Number:</label>
+                      <input
+                        type="tel"
+                        className="w-full px-2 py-1 border-b-2 border-[#a132cd] text-base"
+                        placeholder="Mobile Number (+91-8791XXXXXX)"
+                        pattern="[0-9]{10}"
+                      />
+                    </div>
+                    <div className="flex flex-col gap-5">
+                      <div className="flex items-center gap-3">
+                        <input
+                          type="checkbox"
+                          name="terms"
+                          id="T_C"
+                          defaultChecked
+                        />
+                        <label htmlFor="T_C" className="text-xs">
+                          I agree to the{" "}
+                          <span className="text-[#32cd92] underline cursor-pointer active:text-[#32cd32]">
+                            Terms & conditions
+                          </span>
+                        </label>
+                      </div>
+                      <input
+                        type="submit"
+                        value="Login"
+                        id="logBtn"
+                        name="phone_input"
+                        className="bg-slate-100 w-full py-2 rounded-md cursor-pointer hover:bg-slate-200 active:bg-black active:text-white"
+                      />
+                    </div>
+                  </div>
+                </form>
+                <div className="hidden">
+                  <form action="" method="post">
+                    <div className="flex flex-col space-y-16">
+                      <div className="flex flex-row items-center justify-between mx-auto w-full max-w-xs space-x-3">
+                        {[1, 2, 3, 4].map((index) => (
+                          <div key={index} className="w-16 h-16">
+                            <input
+                              className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                              type="text"
+                              name=""
+                              id=""
+                            />
+                          </div>
+                        ))}
+                      </div>
+                      <div className="flex flex-col space-y-5">
+                        <div className="flex flex-row items-center justify-center text-center text-sm font-medium space-x-1 text-gray-500">
+                          <p>Didn{"'"}t receive the code?</p>
+                          <a
+                            className="text-blue-600"
+                            href="http://"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            Resend
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+
+const SignUp = () => {
+   return (
+    <>
+    Hello
+    </>
+   ) 
+}
+
+
+
+export default LoginPopup;
--- client/src/components/Navigation/Navbar/Navbar.jsx
+++ client/src/components/Navigation/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Logo from "../Logo/Logo";
 import CartFloat from "../../Cart/CartFloat/CartFloat";
 import WishlistPopup from "../../Wishlist/Wishlist";
@@ -7,13 +7,33 @@ import Dropdown from "../Dropdown/Dropdown";
 import { CiHeart, CiShoppingCart } from "react-icons/ci";
 import { HiBars3BottomLeft } from "react-icons/hi2";
 import BackBtn from "../../BackBtn/BackBtn";
+import LoginPopup from "../../Authantication/LoginPopup";
+import { motion } from "framer-motion"; // Import motion
 
+// eslint-disable-next-line react/prop-types
 function Navbar() {
+  const links = [
+    "About Us",
+    "Track Order",
+    "Help",
+    "Verified Refurbished",
+    "Student Special",
+    "Deals",
+    "iPhone",
+    "OnePlus",
+    "Samsung Galaxy",
+    "MI",
+    "VIVO",
+    "OPPO",
+  ];
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isLogOpen, setIsLogOpen] = useState(false);
   const [isWishlistOpen, setIsWishlistOpen] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const toggleCart = () => setIsCartOpen(!isCartOpen);
+  const openLogin = () => setIsLogOpen(true);
+  const closeLogin = () => setIsLogOpen(false);
   const toggleWishlist = () => setIsWishlistOpen(!isWishlistOpen);
 
   const toggleSidebar = () => {
@@ -29,6 +49,15 @@ function Navbar() {
     // Add code to handle option selection
   };
 
+  const sidebarVariants = {
+    hidden: { x: -300 }, // Slide the sidebar out of view
+    visible: { x: 0 },   // Slide the sidebar into view
+  };
+  const sidebarContentVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+  
 
   return (
     <>
@@ -45,52 +74,51 @@ function Navbar() {
             </div>
           </div>
           <ul className="list-none hidden md:flex justify-between items-center text-[8px] md:text-[12px] lg:text-xs xl:text-xs 2xl:text-sm gap-2 md:gap-5  cursor-pointer">
-              <li>About Us</li>
-              <li>Track Order</li>
-              <li>Help</li>
-            </ul>
-          {isSidebarOpen && ( // Conditionally render the sidebar content
-            <>
-            
-              <div className="md:hidden fixed top-0 left-0 h-full w-64 bg-white z-50 border-r border-gray-200">
-              <BackBtn func={handleSidebarOptionClick} className={"left-0 m-5 text-base "}/>
-                <ul className="p-4">
-                  <li className="my-2 flex items-center justify-center">About Us</li>
-                  <hr className="h-px border-0 my-1 bg-gray-200"/>
-                  <li className="my-2 flex items-center justify-center">Track Order</li>
-                  <hr className="h-px border-0 my-1 bg-gray-200"/>
-                  <li className="my-2 flex items-center justify-center">Help</li>
-                  <hr className="h-px border-0 my-1 bg-gray-200"/>
-                  <li className="my-2 flex items-center justify-center">Verified Refurbished</li>
-                  <hr className="h-px border-0 my-1 bg-gray-200"/>
-                  <li className="my-2 flex items-center justify-center">Student Special</li>
-                  <hr className="h-px border-0 my-1 bg-gray-200"/>
-                  <li className="my-2 flex items-center justify-center">Deals</li>
-                  <hr className="h-px border-0 my-1 bg-gray-200"/>
-                  <li className="my-2 flex items-center justify-center">iPhone</li>
-                  <hr className="h-px border-0 my-1 bg-gray-200"/>
-                  <li className="my-2 flex items-center justify-center">OnePlus</li>
-                  <hr className="h-px border-0 my-1 bg-gray-200"/>
-                  <li className="my-2 flex items-center justify-center">Samasung Galaxy</li>
-                  <hr className="h-px border-0 my-1 bg-gray-200"/>
-                  <li className="my-2 flex items-center justify-center">MI</li>
-                  <hr className="h-px border-0 my-1 bg-gray-200"/>
-                  <li className="my-2 flex items-center justify-center">VIVO</li>
-                  <hr className="h-px border-0 my-1 bg-gray-200"/>
-                  <li className="my-2 flex items-center justify-center">OPPO</li>
-                  <hr className="h-px border-0 my-1 bg-gray-200"/>
-                </ul>
-              </div>
-            </>
-          )}
+            <li>About Us</li>
+            <li>Track Order</li>
+            <li>Help</li>
+          </ul>
+          {isSidebarOpen && (
+  <motion.div
+    className="md:hidden fixed top-0 left-0 h-full w-64 bg-white z-50 border-r border-gray-200"
+    initial="hidden"
+    animate={isSidebarOpen ? "visible" : "hidden"}
+    exit="hidden"
+    variants={sidebarVariants}
+  >
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={sidebarContentVariants}
+    >
+      <BackBtn
+                func={handleSidebarOptionClick}
+                className={"left-0 m-5 text-base "}
+              />
+              {links.map((link, index) => (
+                <React.Fragment key={link}>
+                  <li className="my-2 flex items-center justify-center">{link}</li>
+                  {index < links.length - 1 && (
+                    <hr className="h-px border-0 my-1 bg-gray-200" />
+                  )}
+                </React.Fragment>
+              ))}
+    </motion.div>
+  </motion.div>
+)}
           <div className="md:w-1/5 lg:w-2/5 xl:w-3/6 2xl:w-3/6 hidden md:flex">
             <Search />
           </div>
-
           <div className="flex justify-between items-center gap-5 mx-5 md:mx-0 md:gap-10">
             <CiHeart className="hover:scale-105 ease-linear cursor-pointer text-2xl 2xl:text-3xl" />
-            <CiShoppingCart className="hover:scale-105 ease-linear cursor-pointer text-2xl 2xl:text-3xl" />
-            <button className="px-1 py-.5 md:px-3 h-5 md:h-9 border border-black rounded-[.25rem] md:rounded-md lg:rounded-md xl:rounded-lg active:bg-black active:text-white hover:bg-gray-200 text-[10px] md:text-[12px] lg:text-sm xl:text-sm hover:scale-105">
+            <CiShoppingCart
+              onClick={toggleCart}
+              className="hover:scale-105 ease-linear cursor-pointer text-2xl 2xl:text-3xl"
+            />
+            <button
+              onClick={openLogin}
+              className="px-1 py-.5 md:px-3 h-5 md:h-9 border border-black rounded-[.25rem] md:rounded-md lg:rounded-md xl:rounded-lg active:bg-black active:text-white hover:bg-gray-200 text-[10px] md:text-[12px] lg:text-sm xl:text-sm hover:scale-105"
+            >
               Login
             </button>
           </div>
@@ -101,13 +129,14 @@ function Navbar() {
         <div className="hidden md:block">
           <Dropdown />
         </div>
-        <hr className="w-full h-px border-0 bg-gray-400" />
+        <hr className="w-full h-px border-0 bg-gray-200" />
       </div>
       <div className="w-full justify-center items-center md:hidden flex">
         <Search className="w-10/12 py-1.5 my-1" />
       </div>
 
       {isCartOpen && <CartFloat isOpen={isCartOpen} onClose={toggleCart} />}
+      {isLogOpen && <LoginPopup isOpen={isLogOpen} onClose={closeLogin} />}
       {isWishlistOpen && (
         <WishlistPopup
           isWishlistOpen={isWishlistOpen}
