8130aa30782c1da1007ec4eaf924631a99d9a69a
PranshuTyagi2003 <88305222+PranshuTyagi2003@users.noreply.github.com>
2023-10-16 16:57:26 +0530

    nhfhgfgh

--- cms/fonelane/config/middlewares.js
+++ cms/fonelane/config/middlewares.js
@@ -6,13 +6,8 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-<<<<<<< HEAD
           'connect-src': ["'self'", 'https:'],
          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-=======
-          connectSrc: ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
->>>>>>> 1edc2f8fb0cee0ccbef73cc59af3369404c55dda
           'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,
         },
