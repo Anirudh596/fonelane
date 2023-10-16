ab65ef673ea7ee32a33e6c523c76825dacfe8d79
PranshuTyagi2003 <88305222+PranshuTyagi2003@users.noreply.github.com>
2023-10-16 16:29:31 +0530

    strapi changes

--- client/src/pages/PrivacyPolicy/RefundReturn.jsx
+++ client/src/pages/PrivacyPolicy/RefundReturn.jsx
@@ -1,7 +1,33 @@
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import parse from "html-react-parser";
 
 function RefundReturn() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`http://localhost:1337/api/policies/1?populate=*`, {
+          headers: {
+            Authorization: "bearer " + "422d2e9d1a9f0707a1622e0552b49661b6e630c8d02f25c724721eedc0376e8947e98312a4adf3bf21bbc7bee43f269d1471ca84c9f927b05ed421fba03c5217ec35ecd8121e836f96e0f01fe4582de30c62aad923007ae34066f6a443dd2e554cc819db2869212bc54a139c4b28fe55de325cdf9049dd7dbf253b053c56cd14",
+          }
+        });
+
+        // Extract and set product images from the response data
+        
+        const privacydata = res.data.data.attributes.policy;
+        setData(privacydata)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   const [ref, inView] = useInView({
     triggerOnce: true,
     threshold: 0.2, // Adjust this threshold as needed
@@ -46,7 +72,13 @@ function RefundReturn() {
         />
       </div>
       <div>
-        <AccordionCustomIcon />
+      {data.map((item, index) => (
+          <AccordionCustomIcon
+            key={index}
+            heading={item.heading} // Pass the title from the data
+            content={item.content} // Pass the content from the data
+          />
+        ))}
       </div>
     </div>
   );
@@ -84,7 +116,7 @@ function Icon({ id, open }) {
   );
 }
 
-export function AccordionCustomIcon() {
+export function AccordionCustomIcon({ heading, content }) {
   const [open, setOpen] = React.useState(0);
 
   const handleOpen = (value) => setOpen(open === value ? 0 : value);
@@ -93,37 +125,17 @@ export function AccordionCustomIcon() {
     <>
       <Accordion open={open === 1} icon={<Icon id={1} open={open} />}>
         <AccordionHeader onClick={() => handleOpen(1)}>
-          What is Material Tailwind?
+          {heading} {/* Display the title from props */}
         </AccordionHeader>
         <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
-      </Accordion>
-      <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
-        <AccordionHeader onClick={() => handleOpen(2)}>
-          How to use Material Tailwind?
-        </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
-      </Accordion>
-      <Accordion open={open === 3} icon={<Icon id={3} open={open} />}>
-        <AccordionHeader onClick={() => handleOpen(3)}>
-          What can I do with Material Tailwind?
-        </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
+          {parse(content)} {/* Use ReactHtmlParser to render HTML content */}
         </AccordionBody>
       </Accordion>
     </>
   );
 }
+
+
+
+
+
--- cms/fonelane/config/middlewares.js
+++ cms/fonelane/config/middlewares.js
@@ -6,7 +6,8 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          connectSrc: ["'self'", 'http://ec2-52-66-187-61.ap-south-1.compute.amazonaws.com:1337'],
+          connectSrc: ["'self'", 'http://ec2-13-233-105-48.ap-south-1.compute.amazonaws.com:1337'],
+          // 'connect-src': ["'self'", 'https:'],
           'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,
