2c4210ade2e2797e40b46be21b77206dbb033fcc
PranshuTyagi2003 <88305222+PranshuTyagi2003@users.noreply.github.com>
2023-10-16 13:30:09 +0530

    middleware change

--- cms/fonelane/config/middlewares.js
+++ cms/fonelane/config/middlewares.js
@@ -6,7 +6,7 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
+          connectSrc: ["'self'", 'http://ec2-52-66-187-61.ap-south-1.compute.amazonaws.com:1337'],
           'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,
