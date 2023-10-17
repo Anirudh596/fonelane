cddd248f3e4b1c5f50e3da52b6cd3f8a016a4dec
Anirudh596 <anirudhconnect0@gmail.com>
2023-10-17 15:46:38 +0530

    again updating middlewarejs in strapi for https support for netlify

--- cms/fonelane/config/middlewares.js
+++ cms/fonelane/config/middlewares.js
@@ -1,30 +1,35 @@
 module.exports = [
-  'strapi::errors',
+  "strapi::errors",
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          "connect-src": [
+            "'self'",
+            "'unsafe-inline'",
+            "https://ec2-35-154-21-93.ap-south-1.compute.amazonaws.com:1337",
+            "http://ec2-35-154-21-93.ap-south-1.compute.amazonaws.com:1337",
+          ],
+          "img-src": ["data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["data:", "blob:", "res.cloudinary.com"],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
   {
-    name: 'strapi::cors',
+    name: "strapi::cors",
     config: {
-      origin: ['https://fonelane.netlify.app'], // Add your Netlify site URL
+      origin: ["https://fonelane.netlify.app", "http://fonelane.netlify.app"], // Include both http and https versions
     },
   },
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
